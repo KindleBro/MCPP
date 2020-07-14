883a01ba9889f4140d8ee1acdb4589803aed428c
EonZeNx <sparck.va@gmail.com>
2020-07-14 18:25:49 +1200

    Cleanup + Campfire recipe test

--- src/main/java/com/eonzenx/mcppmod/objects/items/AlchemistsGoldIngot.java
+++ src/main/java/com/eonzenx/mcppmod/objects/items/AlchemistsGoldIngot.java
@@ -2,9 +2,11 @@ package com.eonzenx.mcppmod.objects.items;
 
 import com.eonzenx.mcppmod.util.CustomItemGroups;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 
 public class AlchemistsGoldIngot extends Item {
     public AlchemistsGoldIngot() {
         super(new Properties().group(CustomItemGroups.CRAFTING_TAB));
     }
+
 }
